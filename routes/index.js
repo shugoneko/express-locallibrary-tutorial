81e3819779dd1246d131566e97f9fd9e4e0e38ce
shugoneko <ladyshugoneko@yahoo.com>
2019-09-12 13:05:37 -0500

    Added Cool Route
    and set up the stuff

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET cool page. */
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'You are so cool.' });
+});
+
 module.exports = router;
